86fd7adeb87ac7547af4fd4c41d063a135df9647
Yuan_Liu <lyjspx@126.com>
2017-09-24 02:29:43 -0500

    update in diagram

--- Venn diagram/Venn.R
+++ Venn diagram/Venn.R
@@ -1,4 +1,7 @@
 setwd("/Users/desert/Google Drive/Alignment_Project/Breeding_population")
+
+setwd("C:/Users/liu.yuan/Google Drive/Alignment_Project/Breeding_population")
+
 list.files()
 control = read.table("control_GY.txt", header = TRUE,sep = "\t")
 control_marker = control$Marker
@@ -9,9 +12,37 @@ low_marker = low$Marker
 high = read.table("high_GY.txt", header = TRUE, sep = "\t")
 high_marker = high$Marker
 
-control_low = c(low_marker,control_marker)
+###dataset is too large, intersection require 300GB MEM
 
-overlap = calculate.overlap(x = list("control" = low_marker, "low" = low_marker, "high" = high_marker))
-draw.triple.venn(overlap)
+control_low = 0
+for(i in 1:58860){
+  if(control_marker[i] %in% low_marker){
+    control_low = control_low + 1
+  }
+} #28022
 
-interaction(control_marker,low_marker)
+
+control_high = 0
+for(i in 1:length(control_marker)){
+  if(control_marker[i] %in% high_marker){
+    control_high = control_high + 1
+  }
+} #32419
+
+low_high = 0
+for(i in 1:length(low_marker)){
+  if(low_marker[i] %in% high_marker){
+    low_high = low_high + 1
+  }
+} #33710
+
+control_low_high = 0
+for(i in 1:length(control_marker)){
+  if(control_marker[i] %in% high_marker && control_marker[i] %in% low_marker){
+    control_low_high = control_low_high + 1
+  }
+} #23676
+
+draw.triple.venn(area1 = 58860, area2 = 118971, area3 = 371990, 
+                 n12 = 28022,n23 = 33710, n13 = 32419,n123 = 23676,
+                 category = c("Default", "Scatter", "Cluster"))
