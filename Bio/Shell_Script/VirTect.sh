c16fdf22823463f17e117e85662ad3374e5ca0cd
Yuan_Liu <lyjspx@126.com>
2019-02-25 12:29:58 -0600

    Update VirTect.sh

--- Bio/Shell_Script/VirTect.sh
+++ Bio/Shell_Script/VirTect.sh
@@ -1,1 +1,2 @@
 #command for detecting virus integration
+python VirTect.py -t 12 -1 ../ViruSeq/L526401A/L526401A_1.fq  -2 ../ViruSeq/L526401A/L526401A_2.fq  -o test_on_VirusSeq_data/ -ucsc_gene human_reference/gencode.v29.annotation.gtf -index human_reference/GRCh38.p12.genome -index_vir viruses_reference/viruses_757.fasta -d 200
